0ed73ac9abb83848bc39a6fae621562393a9d767
jousethen <jthen93@gmail.com>
2021-08-24 21:25:48 -0400

    Tests pass

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = "*") {
+  return function (adj = "special") {
+    return `You are ${flair}${adj}${flair}!`
+  }
+}
+
+const Calculator = {
+  add: function (a, b) { return a + b },
+  subtract: function (a, b) { return a - b },
+  multiply: function (a, b) { return a * b },
+  divide: function (a, b) { return a / b },
+}
+
+function actionApplyer(startingInt, funcArr) {
+  return funcArr.reduce(function (accum, g) {
+    return g(accum);
+  }, startingInt)
+}
